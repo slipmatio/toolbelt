a5d8a429e71c1c7d92cf07cbb7f714e17baac082
Ville Säävuori <ville@unessa.net>
2024-01-05 18:43:45 +0200

    fix: fix package exports

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'toolbelt',
       fileName: 'toolbelt',
-      formats: ['es', 'cjs'],
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
       external: ['vue', 'vue-router'],
