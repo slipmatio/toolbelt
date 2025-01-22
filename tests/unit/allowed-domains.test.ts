516bfc3b9915de77e45b5a956a0980915d9cd8c2
Ville Säävuori <ville@unessa.net>
2025-01-22 23:46:45 +0200

    feat: added tests for localhost urls

--- tests/unit/allowed-domains.test.ts
+++ tests/unit/allowed-domains.test.ts
@@ -2,13 +2,18 @@ import { expect, test } from 'vitest'
 
 import { isAllowedDomain } from '@/browser'
 
-test('isAllowedDomain', async () => {
-  const allowed = ['www.example.com', 'slipmat.io']
+test('isAllowedDomain', () => {
+  const allowed = ['www.example.com', 'slipmat.io', 'localhost:8000']
 
   expect(isAllowedDomain('https://www.fi', allowed)).toBe(false)
   expect(isAllowedDomain('https://example.com/', allowed)).toBe(false)
   expect(isAllowedDomain('https://notslipmat.io', allowed)).toBe(false)
+
   expect(isAllowedDomain('https://next.slipmat.io/djuninen/', allowed)).toBe(true)
   expect(isAllowedDomain('https://slipmat.io', allowed)).toBe(true)
   expect(isAllowedDomain('https://account.slipmat.io', allowed)).toBe(true)
+
+  expect(isAllowedDomain('http://localhost', allowed)).toBe(false)
+  expect(isAllowedDomain('http://localhost:8000', allowed)).toBe(true)
+  expect(isAllowedDomain('http://localhost:8000/login/', allowed)).toBe(true)
 })
