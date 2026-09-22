1378a29d7c1d88b5ca8b0aaa74219acb5a979c4f
Ville Säävuori <ville@unessa.net>
2026-09-22 21:12:57 +0300

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import viteConfig from './vite.config.ts'
 
 export default mergeConfig(
   viteConfig,
